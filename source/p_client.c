695bb4591f0b68b1001373e9ee4f8ba0269a2457
adean <8dino2@gmail.com>
2022-04-19 16:19:26 -0400

    Finally found that damned var

--- source/p_client.c
+++ source/p_client.c
@@ -1237,7 +1237,7 @@ void ClientObituary(edict_t * self, edict_t * inflictor, edict_t * attacker)
 			message, attacker->client->pers.netname, message2);
 			PrintDeathMessage(death_msg, self);
 			IRC_printf(IRC_T_KILL, death_msg);
-			gi.bprintf(PRINT_STAT, "%s:%s:%s:%s\n", t_id, weapmod, k_id, loc);
+			gi.bprintf(PRINT_STAT, "%s:%s:%s:%s\n", t_id, weapmod, k_id, locmsg);
 
 			AddKilledPlayer(attacker, self);
 
@@ -1258,11 +1258,7 @@ void ClientObituary(edict_t * self, edict_t * inflictor, edict_t * attacker)
 			}
 
 			return;
-		}
-		else
-		{
-			gi.bprintf(PRINT_STAT, "%s:%s:%s:%s\n", t_id, weapmod, k_id, loc);
-		}
+		}	// if(message)
 	}
 
 	sprintf(death_msg, "%s died\n", self->client->pers.netname);
